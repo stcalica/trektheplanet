8c05d2ac9f9d8534a24045d9b56b31ecbd55551d
stcalica <stcalica@gmail.com>
2016-02-09 03:33:55 -0800

    updated README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Website Map Overlay for TrekThePlanet website
 
 
-Will be written using jQuery, Bootstrap, Google-maps API and hopefully Django Python! 
+Written using jQuery, Bootstrap, Google-maps API and hopefully Django Python! 
 
 
 This website will track and log the travels and expenses of TechnoHippy and CyberPunk
@@ -12,4 +12,30 @@ a video diary listing from both of us. Along with highlight videoes.
 The site will allow people to donate to us and/or offer advice. 
 
 Probably will add facebook functionality or sometype of commenting system to be able to talk to visitors. 
+
+
+To get started: 
+
+SHOWN HOW TO IN LINUX
+
+start up the virtual enviroment: 
+
+source ./env/bin/activate 
+
+INSTALL PYTHON DEPENDENCIES: 
+
+pip install requirements.txt 
+   - don't use sudo or you will be installing dependencies to your computer and not the virtual enviroment
+
+Make migrations and create superuser: 
+
+	python manage.py makemirgrations 
+	python manage.py migrate
+	python manage.py createsuperuser
+		-follow any prompts 
+
+
+Run the server: 
+	python manage.py runserver
+
 
--- trektheplanet/mapper/helper.py
+++ trektheplanet/mapper/helper.py
@@ -5,35 +5,53 @@ from django.core import serializers
 from .models import Destination
 import json
 
+""" Collect all coordinates and serve it out """
+
 def collect_coordinates():
 	s = serializers.serialize("json", Destination.objects.filter(approved=True))
 	dests = json.loads(s)
 	#coordinates = []
 	return dests
 	#return coordinates
+""" Serve a International Path """
 
 def compute_international():
 #computes the best way to connect the strongly connected components that make up each country
 #connecting country to country
 #returns the path of the countries connections
 	s = serializers.serialize("json", Destination.objects.filter(base=True))
-	path = json.loads(s) 
+	path = json.loads(s)
+	nodes = Destination.objects.filter(base=True)
+	for node in nodes:	
+		print node   
 	print(json.dumps(path)) 
 #the default of this is just selecting the coordinates that are marked as a base in each country
 	return path
 
+""" Suggested Travels  """
 
+def travel_by_price():
+	pass
+
+def travel_by_less():
+	#travel by less edges
+	pass
+
+def travel_by_more():
+	#travel by more edges
+	pass
 
 	
 	
-	
+""" Travel API data serve as strings """ 	
 def collect_travel_data(coor): 
 	pass
+
+""" Compute costs and paths for helper functions  """
 	
 def compute_cost_path(coor): 
 	pass
 
-	
 def compute_distance_path(coor):
 	pass
 	
